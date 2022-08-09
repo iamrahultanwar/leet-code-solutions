01eb3891139b9aea42116779ec44f94819306920
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-10 00:13:39 +0530

    Time: 41 ms (66.93%), Space: 13.8 MB (57.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,17 +1,26 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        currentNumberOfWays = 0
-        waysToTop = [1]
+    def climbStairs(self,n:int)->int:
+        one,two = 1,1
+        
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+            
+        return one
+#     def climbStairs(self, n: int) -> int:
+#         currentNumberOfWays = 0
+#         waysToTop = [1]
 
-        for currentHeight in range(1,n+1):
-            startOfWindow = currentHeight - 3
-            endOfWindow = currentHeight - 1
+#         for currentHeight in range(1,n+1):
+#             startOfWindow = currentHeight - 3
+#             endOfWindow = currentHeight - 1
 
-            if startOfWindow >= 0:
-                currentNumberOfWays -= waysToTop[startOfWindow]
+#             if startOfWindow >= 0:
+#                 currentNumberOfWays -= waysToTop[startOfWindow]
 
-            currentNumberOfWays += waysToTop[endOfWindow]
-            waysToTop.append(currentNumberOfWays)
+#             currentNumberOfWays += waysToTop[endOfWindow]
+#             waysToTop.append(currentNumberOfWays)
 
-        return waysToTop.pop()
+#         return waysToTop.pop()
         

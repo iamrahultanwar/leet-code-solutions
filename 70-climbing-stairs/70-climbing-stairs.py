66ad090cd2b5752176884cc735f0e2e41f860e32
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-10 00:10:49 +0530

    Time: 38 ms (75.13%), Space: 13.9 MB (57.10%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        currentNumberOfWays = 0
+        waysToTop = [1]
+
+        for currentHeight in range(1,n+1):
+            startOfWindow = currentHeight - 3
+            endOfWindow = currentHeight - 1
+
+            if startOfWindow >= 0:
+                currentNumberOfWays -= waysToTop[startOfWindow]
+
+            currentNumberOfWays += waysToTop[endOfWindow]
+            waysToTop.append(currentNumberOfWays)
+
+        return waysToTop.pop()
+        

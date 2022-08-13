170df8d2dea68679590b11469277cba0a60dec86
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-13 08:41:13 +0530

    Time: 2624 ms (76.19%), Space: 59.3 MB (33.20%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -1,11 +1,17 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        # idea to to filter out the not valid triplet
+        
         good = set()
-
+        
         for t in triplets:
             if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
                 continue
-            for i, v in enumerate(t):
+                
+                
+            for i,v in enumerate(t):
                 if v == target[i]:
                     good.add(i)
+                    
         return len(good) == 3
+       

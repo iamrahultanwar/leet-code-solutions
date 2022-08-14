fff4acc68aa4fd6a1a4b51cc81822c3aa0205970
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-14 12:32:16 +0530

    Time: 6937 ms (12.27%), Space: 13 MB (99.07%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,27 +1,29 @@
 class Solution(object):
     def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        # 0. base case
-        if len(s) == 1:
+        # handle base case
+        
+        if len(s)==1:
             return s
-        # 1. define max_str, and set up the nested loop
-        max_str = ""
+        
+        
+        maxString = ""
+        
+        
+        # idea 
+        
         for i in range(len(s)-1):
-            for j in range(i+1, len(s)):
-                # if the ith element == jth element, get the substring and see if its palindromic
+            for j in range(i,len(s)):
                 if s[i] == s[j]:
-                    # get the substring
                     sub = s[i:j+1]
-                    # only if the substring len is > max_str length we want to reverse
-                    if len(sub) > len(max_str):
+                    if len(sub) > len(maxString):
                         if sub == sub[::-1]:
-                            max_str = sub
-        # if there were no palindromes, return s[0]
-        if len(max_str) == 0:
-            return s[0]
-        return max_str
+                            maxString = sub
+        
+        if len(maxString) ==1:
+            return maxString[0]
+        
+        return maxString
+
+        
 
  

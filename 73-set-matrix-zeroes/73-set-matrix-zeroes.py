1ea73fa52a9cfbfd6bbfa25e2d74d4ebaef86d70
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-15 15:16:49 +0530

    Time: 295 ms (6.51%), Space: 14.7 MB (90.71%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -0,0 +1,46 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        ROWS,COLS = len(matrix),len(matrix[0])
+        
+        visited = set()
+        
+        def dfs(r,c):
+            for row in range(r+1,ROWS):
+                matrix[row][c] = 0
+            
+            for row in range(r):
+                matrix[row][c] = 0
+                
+            for col in range(c+1,COLS):
+                matrix[r][col] = 0
+                
+            for col in range(c):
+                matrix[r][col] = 0
+            
+        zeros = []
+        
+        
+        
+        for r in range(ROWS):
+            for c in range(COLS):         
+                if matrix[r][c] == 0:
+                    zeros.append((r,c))
+                    
+                    
+        while zeros:
+            row,col =  zeros.pop()
+            dfs(row,col)
+        
+                    
+                    
+        return matrix
+    
+    
+    
+    [[0,1,2,0],
+     [3,4,5,2],
+     [1,3,1,5]]

cdeba5fc191dcb71506d093fd37eba802832156f
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-21 11:28:11 +0530

    Time: 99 ms (54.00%), Space: 14.4 MB (46.51%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,33 +1,35 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        posDia = set() # holds positive diagonals
+        negDia = set() # holds negative diagonals
         col = set()
-        posDiag = set()  # (r + c)
-        negDiag = set()  # (r - c)
-
-        res = []
+        result = []
+        
         board = [["."] * n for i in range(n)]
-
+        
+        
         def backtrack(r):
+            
             if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
+                result.append(["".join(row) for row in board])
                 return
-
+            
             for c in range(n):
-                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                
+                if c in col or (r + c) in posDia or (r - c) in negDia:
                     continue
-
+                    
+                posDia.add(r+c)
+                negDia.add(r-c)
                 col.add(c)
-                posDiag.add(r + c)
-                negDiag.add(r - c)
                 board[r][c] = "Q"
-
-                backtrack(r + 1)
-
+                backtrack(r+1)
+                posDia.remove(r+c)
+                negDia.remove(r-c)
                 col.remove(c)
-                posDiag.remove(r + c)
-                negDiag.remove(r - c)
                 board[r][c] = "."
-
+                
+                
         backtrack(0)
-        return res
+        
+        return result

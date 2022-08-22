c850d04a01ec66015a59ea466300d42d9d789121
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-22 12:36:07 +0530

    Time: 806 ms (75.10%), Space: 30.1 MB (65.68%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,23 +1,25 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res, part = [], []
-
-        def dfs(i):
+        
+        def isValid(l,r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l, r = l + 1, r - 1
+            return True
+            
+        result = []
+        
+        def backtrack(i,part):
             if i >= len(s):
-                res.append(part.copy())
+                result.append(part.copy())
                 return
             for j in range(i, len(s)):
-                if self.isPali(s, i, j):
+                if isValid(i, j):
                     part.append(s[i : j + 1])
-                    dfs(j + 1)
+                    backtrack(j + 1,part)
                     part.pop()
-
-        dfs(0)
-        return res
-
-    def isPali(self, s, l, r):
-        while l < r:
-            if s[l] != s[r]:
-                return False
-            l, r = l + 1, r - 1
-        return True
+                    
+        backtrack(0,[])
+        
+        return result

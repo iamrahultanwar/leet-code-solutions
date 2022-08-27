16dadb067c72493b93b1cfff021ffa675544ca70
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-27 10:17:42 +0530

    Time: 60 ms (22.85%), Space: 14 MB (19.71%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,18 +1,47 @@
 class Solution:
+    def dpWithMemo(self,nums:List[int]) -> int:
+        
+        dp = [-1] * len(nums)
+        
+        maxNumber = [-1]
+        
+        def backtrack(i):
+            
+            if i >= len(nums):
+                return 0
+            
+            if dp[i] > -1 : return dp[i]
+            
+            take = backtrack(i+2) + nums[i]
+            notTake = backtrack(i+1)
+            
+            dp[i] = max(take,notTake)
+            
+            maxNumber[0] = max(maxNumber[0],dp[i])
+            return dp[i]
+        
+        backtrack(0)
+        
+        return maxNumber[0]
+        
     def rob(self, nums: List[int]) -> int:
+        
+        return self.dpWithMemo(nums)
         # 1->3,1
         # 2->1
         
         #2->9,3,1
         
-        rob1,rob2 = 0,0
+#         rob1,rob2 = 0,0
         
-        for n in nums:
-            temp = max(rob1+n,rob2)
-            rob1 = rob2
-            rob2 = temp
+#         for n in nums:
+#             temp = max(rob1+n,rob2)
+#             rob1 = rob2
+#             rob2 = temp
             
             
-        return rob2
+#         return rob2
     
         
+        
+        # [1,2,3,1]

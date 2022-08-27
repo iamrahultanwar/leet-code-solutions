11d1afc72169df10066288dd907c476134367bab
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-27 23:43:27 +0530

    Time: 56 ms (31.36%), Space: 14 MB (15.12%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,5 +1,24 @@
 class Solution:
+    def tabu(self,m:int,n:int)->int:
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                
+                up = dp[i-1][j] if i > 0 else 0
+                left = dp[i][j-1] if j > 0 else 0
+                
+                dp[i][j] = up + left
+                
+                
+        return dp[-1][-1]
+    
     def uniquePaths(self, m: int, n: int) -> int:
+        
+        return self.tabu(m,n)
         
         if m == 1 and n == 1:
             return 1

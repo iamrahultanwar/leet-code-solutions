d42fb89fcc06f4c1927525b7ed7db711b19af4a8
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-27 10:22:57 +0530

    Time: 48 ms (55.30%), Space: 13.9 MB (19.71%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -37,22 +37,4 @@ class Solution:
         
     def rob(self, nums: List[int]) -> int:
         
-        return self.dpWithMemo(nums)
-        # 1->3,1
-        # 2->1
-        
-        #2->9,3,1
-        
-#         rob1,rob2 = 0,0
-        
-#         for n in nums:
-#             temp = max(rob1+n,rob2)
-#             rob1 = rob2
-#             rob2 = temp
-            
-            
-#         return rob2
-    
-        
-        
-        # [1,2,3,1]
+        return self.dpWithTabu(nums)

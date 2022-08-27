01ac0f1c64304f12f55a67dc296dbfb72c537771
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-27 10:22:37 +0530

    Time: 64 ms (15.12%), Space: 13.9 MB (66.15%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -23,6 +23,17 @@ class Solution:
         backtrack(0)
         
         return maxNumber[0]
+    
+    def dpWithTabu(self,nums:List[int]) -> int:
+        
+        rob1,rob2 = 0,0 # rob1 is take and rob2 is not take
+        
+        for n in nums:
+            temp = max(rob1+n,rob2)
+            rob1 = rob2
+            rob2 = temp
+            
+        return rob2
         
     def rob(self, nums: List[int]) -> int:
         

35525231c4847f83c1f380a255357b5afc902ac1
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-27 23:57:28 +0530

    Time: 73 ms (5.17%), Space: 14.1 MB (10.74%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,5 +1,26 @@
 class Solution:
+    def tabuWithSpace(self,m:int,n:int)->int:
+        
+        prev = [0] * n
+        
+        for i in range(m):
+            curr = [0] * n
+            for j in range(n):
+                if i == 0 and j == 0:
+                    curr[j] = 1
+                else:
+                    up = prev[j] if i > 0 else 0
+                    left = curr[j-1] if j > 0 else 0
+
+                    curr[j] = up + left
+            
+            prev = curr
+            
+        return prev[-1]
+    
     def tabu(self,m:int,n:int)->int:
+        return self.tabuWithSpace(m,n)
+    
         dp = [[0 for _ in range(n)] for _ in range(m)]
         dp[0][0] = 1
         

f067b52c50a87a5ef51d208f1778ece279521397
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-08-29 15:05:07 +0530

    Time: 125 ms (29.03%), Space: 15.3 MB (23.41%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -3,7 +3,7 @@ class Solution:
         
         n = len(triangle)
         
-        dp = [[-1 for _ in range(n)] for _ in range(n)]
+        dp = [[0 for _ in range(n)] for _ in range(n)]
         
         def backtrack(i,j):
             if i == n-1: return triangle[i][j]      
@@ -16,8 +16,16 @@ class Solution:
             dp[i][j] = min(down,downRight)
             return dp[i][j]
         
+        for j in range(n):
+            dp[n-1][j] = triangle[n-1][j]
         
-        return backtrack(0,0)
+        for i in reversed(range(n-1)):
+            for j in range(i+1):
+                down = triangle[i][j] + dp[i+1][j]
+                downRight = triangle[i][j] + dp[i+1][j+1]
+                dp[i][j] = min(down,downRight)
+        
+        return dp[0][0]
             
             
             

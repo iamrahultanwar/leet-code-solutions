f4de54a9e4bf33f6759a0e6e91b9e750026c8ae4
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-09-03 01:14:41 +0530

    Time: 77 ms (53.60%), Space: 13.9 MB (73.91%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -18,10 +18,35 @@ class Solution:
             num  += digit[s] * (10**(idx+1))
         
         return num
+    
+    def numToString(self,num):
+        digit = {
+          0: "0",
+          1: "1",
+          2: "2",
+          3: "3",
+          4: "4",
+          5: "5",
+          6: "6",
+          7: "7",
+          8: "8",
+          9: "9",
+
+        }
+        string = ""
+        if num == 0:
+            string = digit[0]
+        while num:
+            string = digit[(num%10)] + string
+            num = num // 10
+            
+
+        return string
+            
+        
 
     def multiply(self, num1: str, num2: str) -> str:
         
         num1 = self.stringToNum(num1)
         num2 = self.stringToNum(num2)
-        
-        return str(num1 * num2)
+        return self.numToString(num1 * num2)

85bd33bb41b364786f9aac9b6d4e8e3d1bf4f097
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-09-16 00:15:51 +0530

    Time: 271 ms (10.52%), Space: 15.4 MB (17.84%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,11 +1,10 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
-        
+
+    def majorityElement(self, nums):
         if not nums:
             return []
-                
-        count1, count2, candidate1, candidate2 = 0, 0, None, None
         
+        count1, count2, candidate1, candidate2 = 0, 0, None, None
         for n in nums:
             if candidate1 == n:
                 count1 += 1
@@ -20,11 +19,10 @@ class Solution:
             else:
                 count1 -= 1
                 count2 -= 1
-                
-        result =[]
-        for c in [candidate1,candidate2]:
+        
+        result = []
+        for c in [candidate1, candidate2]:
             if nums.count(c) > len(nums)//3:
                 result.append(c)
-                
-                
+
         return result

4b386eb133912b91a2b5602261bbbb895656b132
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-09-17 00:02:03 +0530

    Time: 2028 ms (35.22%), Space: 14.3 MB (9.62%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -1,26 +1,37 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        def findNsum(l, r, target, N, result, results):
-            if r-l+1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:  # early termination
-                return
-            if N == 2: # two pointers solve sorted 2-sum problem
-                while l < r:
-                    s = nums[l] + nums[r]
-                    if s == target:
-                        results.append(result + [nums[l], nums[r]])
-                        l += 1
-                        while l < r and nums[l] == nums[l-1]:
-                            l += 1
-                    elif s < target:
-                        l += 1
-                    else:
-                        r -= 1
-            else: # recursively reduce N
-                for i in range(l, r+1):
-                    if i == l or (i > l and nums[i-1] != nums[i]):
-                        findNsum(i+1, r, target-nums[i], N-1, result+[nums[i]], results)
-
+        
         nums.sort()
-        results = []
-        findNsum(0, len(nums)-1, target, 4, [], results)
-        return results
+        
+        result,quad = [],[]
+        
+        def kSum(k,start,target):
+            
+            if k!=2:
+                for i in range(start,len(nums)-k+1):
+                    if i > start and nums[i] == nums[i-1]:
+                        continue
+                    quad.append(nums[i])
+                    kSum(k-1,i+1,target-nums[i])
+                    quad.pop()
+                    
+                return
+            
+            left,right = start,len(nums)-1
+            
+            while left < right:
+                if nums[left] + nums[right] < target:
+                    left += 1
+                elif nums[left] + nums[right] > target:
+                    right -= 1                  
+                else:
+                    result.append(quad+[nums[left],nums[right]])
+                    left += 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left +=1
+                        
+                        
+        kSum(4,0,target)
+        
+        return result
+                

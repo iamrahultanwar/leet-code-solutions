0e04cd36c663ec12e51328bf89acbda4bc8a58ae
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-09-19 23:06:54 +0530

    Time: 72 ms (27.94%), Space: 14 MB (38.08%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -4,17 +4,16 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        num1Copy = nums1[:m]
-        p1,p2 = 0,0
+        p1 = m - 1
+        p2 = n - 1
         
-        for p in range(m+n):
-            if p2 >= n or (p1 < m and num1Copy[p1] <= nums2[p2]):
-                nums1[p] = num1Copy[p1]
-                p1 += 1
+        for p in range(m+n-1,-1,-1):
+            if p2 < 0:
+                break
+                
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
             else:
                 nums1[p] = nums2[p2]
-                p2 += 1
-                
-                
-        return nums1
-        
+                p2 -= 1

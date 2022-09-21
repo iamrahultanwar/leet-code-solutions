1ff44f9f809ef96162a51a8ea06961491b1a5662
Rahul Tanwar <mr.rahultanwar@gmail.com>
2022-09-21 17:01:37 +0530

    Time: 141 ms (39.03%), Space: 51.6 MB (67.30%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -2,25 +2,17 @@
  * @param {number[]} prices
  * @return {number}
  */
-const Max = (a,b)=>{
-    if(a > b){
-        return a
-    }
-    return b
-}
+
+const maxNum = (a,b) => a > b ? a : b;
 var maxProfit = function(prices) {
-    let left = 0
-    
-    let result = 0
-    
-    for(let right=1;right < prices.length;right++){
-        
-        if(prices[right] < prices[left]){
-            left = right
+   let left = 0;
+    let right = 0;
+    let maxPrice = 0;
+    for(right = 1;right<prices.length;right++) {
+        if(prices[right] < prices[left]) {
+            left = right;
         }
-        
-        result = Max(result,prices[right]-prices[left])
+        maxPrice = maxNum(maxPrice,prices[right]- prices[left])
     }
-    
-    return result
-};
+    return maxPrice;
+}
